ffca5efc29c57f13bf9ba324a9c4c2959ae9d00c
Sarah Spall <sarah@Actiontec.(none)>
2013-11-27 10:50:04 -0700

    added lcd_init_module

--- arch/x86/lcd/lcd.h
+++ arch/x86/lcd/lcd.h
@@ -1,6 +1,8 @@
 #ifndef LCD_LCD_H
 #define LCD_LCD_H
 
-int lcd_move_module(unsigned long module_start, size_t size);
+int lcd_move_module(struct module *mod);
+int lcd_init_module(void __user * umod, unsigned long len, const char __user * uargs);
+int lcd_load_module(struct load_info *info, const char __user *uargs, int flags);
 
 #endif

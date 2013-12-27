beed5c12792eb1f444404914a6fc56bb6d776afd
Jintack Lim <jintack.lim@utah.edu>
2013-12-26 20:17:13 -0700

    Add capability delete and search function

--- xcap/cap.h
+++ xcap/cap.h
@@ -1,12 +1,5 @@
 
-typedef enum {
-    TYPE_ENDPOINT,
-    TYPE_ASYNCENDPOINT,
-    TYPE_TCB,
-    TYPE_CNODE
-}objType_t;
-
-typedef enum {ObjType_Untyped, ObjType_CNode=777, ObjType_EndPoint, ObjType_Null} LCD_Type;
+typedef enum {ObjType_Untyped, ObjType_CNode=777, ObjType_EndPoint=888, ObjType_Null} LCD_Type;
 
 /* caps with fixed slot potitions in the root CNode */
 enum {
@@ -19,17 +12,11 @@ enum {
 
 typedef uint32_t LCD_Word;
 
-/* 
-An address in the capability space refers to a slot which may or may not contain a capability
-A capability address is stored in a CPointer
-*/
 typedef LCD_Word LCD_CPtr;
 
 typedef LCD_CPtr LCD_CNode;
 typedef LCD_CPtr LCD_Untyped;
 typedef LCD_CPtr LCD_TCB;
-
-int LCD_Untyped_Retype(LCD_Untyped _service, LCD_Type objType, int size_bits, LCD_CNode root, int node_index, int node_depth, int node_offset, int num_objects);
 
 /// Sets the specified number of low-order bits to 1
 #define MASK(bits)      ((1UL << bits) - 1)
@@ -87,49 +74,16 @@ struct cte
 	struct capability cap;
 };
 
+/* internal functions */
+int caps_lookup_slot(struct capability *cnode_cap, LCD_CPtr cptr, uint8_t vbits, uint8_t resolved_bits, struct cte **ret, CapRights rights);
 struct cte * caps_locate_slot(struct cte * topCte, size_t offset);
 void print_cnode(struct cte * myCte, int depth);
-/*
-static int LCD_TCB_SetSpace(LCD_CPtr LCD_CapInitThreadCNode);
-static int LCD_Create_Object(objType_t type);
+LCD_CPtr create_CNode(LCD_Untyped _service, int size_bits, LCD_CNode root, int node_index, int node_depth, int node_offset, int num_objects);
+LCD_CPtr create_EndPoint(LCD_Untyped _service, int size_bits, LCD_CNode root, int node_index, int node_depth, int node_offset, int num_objects);
+int cnode_create_internal(struct capability *src_cap, int size_bits);
 
-*/
-#if 0
-#define IWANA_MAX_LEVELS  3
-
-struct iwana_cspace {
-	/* The caps to the current vertical slice of the cspace we are allocating
-	 * out of. */
-	struct {
-		seL4_CPtr cptr;
-		int offset;
-	} cnodes[IWANA_MAX_LEVELS];
-
-	/* Size (in bits) of each level in the CNode. */
-	int depths[IWANA_MAX_LEVELS];
-
-	/*
-	 * Size (in bits) of each level, taking into account guards.
-	 *
-	 * This is used when:
-	 *
-	 *    (i)  We are trying to address a slot in a CNode of this level; and
-	 *
-	 *    (ii) That CNode was possibly created by an external party, with
-	 *         its cap having a guard.
-	 */
-	int guarded_depths[IWANA_MAX_LEVELS];
-
-	/* Upper limit on the size of the CSpace. */
-	unsigned long cspace_size_bits;
-
-	/* Number of guard bits on the top level. */
-	int guard_bits;
-
-	/* Number of levels in the cspace. */
-	int levels;
-
-	/* First free cap available in the client's CSpace. */
-	seL4_CPtr next_cap;
-};
-#endif
+/* externel interface */
+static int LCD_Create_CSpace(void);
+int LCD_Untyped_Retype(LCD_Untyped _service, LCD_Type objType, int size_bits, LCD_CNode root, int node_index, int node_depth, int node_offset, int num_objects);
+static int LCD_CNode_Delete(LCD_CNode _service, int node_index, int node_depth);
+static void *LCD_CNode_Search(LCD_CNode _service, int node_index, int node_depth);

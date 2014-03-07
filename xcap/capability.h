313223831c535bcb2fb6a016fa9be1eed5f135ec
muktesh <muktesh.khole@utah.edu>
2014-03-07 13:29:25 -0700

    Capability Module

--- xcap/capability.h
+++ xcap/capability.h
@@ -1,14 +1,18 @@
 #ifndef __LCD_CAPABILITY_H__
 #define __LCD_CAPABILITY_H__
 
+#include <linux/types.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/fs.h>
 #include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/semaphore.h>
+#include <asm/page.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR ("FLUX-LAB University of Utah");
 
 typedef enum _lcd_cap_type
 {
@@ -29,10 +33,10 @@ enum {
 }; 
 
 typedef uint32_t lcd_cnode; 		// a pointer to the cnode
-typedef uint32_t cap_id			// a locally unique identifier (address within cspace)
+typedef uint32_t cap_id	;    		// a locally unique identifier (address within cspace)
 typedef uint32_t lcd_cnode_entry;	// a pointer to an entry within a cnode
-typedef uint64   lcd_tcb;		// a pointer/handle to the thread contrl block
-typedef uint16   lcd_cap_rights;	// holds the rights associated with a capability.
+typedef uint64_t   lcd_tcb;		// a pointer/handle to the thread contrl block
+typedef uint16_t   lcd_cap_rights;	// holds the rights associated with a capability.
 
 #define CAPRIGHTS_READ          (1 << 0)
 #define CAPRIGHTS_WRITE         (1 << 1)
@@ -45,27 +49,38 @@ typedef uint16   lcd_cap_rights;	// holds the rights associated with a capabilit
 #define CAPRIGHTS_RWX           (CAPRIGHTS_RW | CAPRIGHTS_EXECUTE)
 #define CAPRIGHTS_NORIGHTS      0
 
-struct cap_derivation_list
+typedef struct _cap_derivation_list
 {
   lcd_tcb          remote_TCB;    // reference to the thread which was granted this capability
   cap_id           remote_cid;   // address in the remote threads capability space where this 
 			          // capability is stored.
-  struct cap_derivation_list *next;
-};
+  struct _cap_derivation_list *next;
+}cap_derivation_list;
 
-struct capability
+typedef struct _capability
 {
   cap_id     cid;    // locally unique to each thread
-}
+}capability;
 
-struct capability_internal 
+typedef struct _capability_internal 
 {
 	void          *hobject;      // a pointer to a kernel object
-	cap_grant_db   *cgrant_list; // list of domain ids to whom this capability is granted
+	struct cap_derivation_list   *cgrant_list; // list of domain ids to whom this capability is granted
 	lcd_cap_rights crights;      // specifies the rights the domain has over this capability
-};
+}capability_internal;
 
-struct cap_node
+struct _cap_node;
+
+typedef struct _cte // capability table entry
+{
+    lcd_cap_type ctetype;
+    union{
+          struct _cap_node *cnode;
+          capability_internal *cap;
+    }u;
+}cte;
+
+typedef struct _cap_node
 {
 	struct _guard
 	{
@@ -74,24 +89,18 @@ struct cap_node
 	}guard;
 	uint32_t nfree_slots; /* number of free slots left in this cnode. when this reaches
 				  2 power bits - 1 we must allocate another cnode before insert*/
-	struct cte *cap_entry; /* may point to another cnode or to a capability */
-	uint16 bits; /* total slots in this cnode is 2 power bits. 
+	cte *cap_entry; /* may point to another cnode or to a capability */
+	uint16_t bits; /* total slots in this cnode is 2 power bits. 
 			Minimum value must be 5 i.e. 32 slots*/
-};
+}cap_node;
 
-struct cte // capability table entry
-{
-	lcd_cap_type ctetype;
-	union{
-	      struct cap_node *cnode;
-	      struct capability_internal *cap;
-	}u;
-};
 
-struct cap_space
+
+typedef struct _cap_space
 {
 	struct cap_node *cnode;
-};
+    struct semaphore g_cspace;
+}cap_space;
 
 
 /* Helper Functions */
@@ -102,7 +111,7 @@ struct cap_space
 cap_id lcd_lookup_free_slot();
 
 // will be used to allocate memory for a cnode.
-cap_node * lcd_create_cnode(uint16 size);
+cap_node * lcd_create_cnode(uint16_t size);
 
 // will be used to de-allocate memory of a cnode.
 uint32_t lcd_delete_cnode(cap_id cid);
@@ -117,34 +126,30 @@ uint32_t lcd_delete_cnode(cap_id cid);
 // one for its cspace
 // one for the endpoint.
 // the returned value goes into the TCB of the caller.
-cap_space * lcd_create_cspace();
+cap_space * lcd_create_cspace(void * ptcb);
 
 // creates a new capability, inserts it into cspace of caller and 
 // returns the capability identifier.
 // it is unclear who will have the right to perform this operation.
-cap_id lcd_create_cap(lcd_tcb ptcb, uint64 hobject, lcd_cap_rights crights);
+cap_id lcd_create_cap(void * ptcb, void * hobject, lcd_cap_rights crights);
 
 // will be used to grant a capability to another thread
 // returns the address of the capability within the cspace of the receiver thread.
 // a logical AND of the crights and rights on the capability being granted will decide the 
 // final rights the granted capability will have.
-cap_id lcd_cap_grant(lcd_tcb dst_tcb, cap_id src_cid, lcd_cap_rights crights);
+cap_id lcd_cap_grant(void * dst_tcb, cap_id src_cid, lcd_cap_rights crights);
 
 // will be called to delete a particular capability in the calling threads 
 // cspace. threads have right to delete capabilities in their own cspace.
-uint32_t lcd_cap_delete(lcd_tcb ptcb, cap_id cid);
+uint32_t lcd_cap_delete(void * ptcb, cap_id cid);
 
 // will be called to delete the capability and all its children.
 // the children can be present in cspace belonging to different threads.
 // as such the thread owning the parent capability has a right to delete 
 // a capability which is its child or was derieved from it.
-uint32_t lcd_cap_revoke(lcd_tcb ptcb, cap_id cid);
-
-// will be used to retype an untyped capability.
-// not clear on the implementation of this function
-uint32_t lcd_untyped_retype();
+uint32_t lcd_cap_revoke(void * ptcb, cap_id cid);
 
 // will be used to get the rights available with a capability.
-uint32_t lcd_get_cap_rights(lcd_tcb ptcb, cap_id cid, lcd_cap_rights &rights);
+uint32_t lcd_get_cap_rights(void * ptcb, cap_id cid, lcd_cap_rights *rights);
 
 #endif // __LCD_CAPABILITY_H__

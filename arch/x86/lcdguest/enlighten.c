bea9baecac7ccfff2b3299be8c609ff01fc3d9e9
Anton Burtsev <aburtsev@flux.utah.edu>
2014-05-16 22:33:15 -0600

    Early attempt to add LCD_GUEST arch

---
+++ arch/x86/lcdguest/enlighten.c
@@ -0,0 +1,12 @@
+/*
+ * Core of LCD paravirt_ops implementation.
+ *
+ * Anton Burtsev <aburtsev@flux.utah.edu>, University of Utah 2014
+ */
+
+/* First C function to be called on Xen boot */
+asmlinkage void __init lcd_start_kernel(void)
+{
+	return;
+}
+

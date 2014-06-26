aa50347d8fd6833176dfdaacbb390a022a33b80f
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2014-06-26 11:44:17 -0600

    Test for gva init.

--- virt/lcd-domains/lcd-tests.c
+++ virt/lcd-domains/lcd-tests.c
@@ -21,10 +21,35 @@ static int test01(void)
 	return 0;
 }
 
+static int test02(void)
+{
+	struct lcd *lcd;
+	int ret;
+
+	ret = lcd_create(&lcd);
+	if (ret) {
+		printk(KERN_ERR "lcd test: test02 failed to create lcd\n");
+		return ret;
+	}
+	
+	ret = lcd_mm_gva_init(lcd, LCD_ARCH_FREE,
+			LCD_ARCH_FREE + 4 * (1 << 20));
+	if (ret) {
+		printk(KERN_ERR "lcd test: test02 failed to init gva\n");
+		return ret;
+	}
+
+	lcd_destroy(lcd);
+
+	return 0;
+}
+
 
 static void lcd_tests(void)
 {
 	if (test01())
+		return;
+	if (test02())
 		return;
 	return;
 }

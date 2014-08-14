72f4d929c2d04c8dec777f00434fdef2ee7b7402
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2014-08-14 09:17:25 -0600

    Two basic tests for caps.

--- drivers/lcd-prototype/api/api-tests.c
+++ drivers/lcd-prototype/api/api-tests.c
@@ -6,14 +6,37 @@
  * Copyright: University of Utah
  */
 
+#define LCD_FAIL(msg...) do{ LCD_ERR(msg); return -1; } while(0)
+#define LCD_PASS() do{ return 0; } while(0)
+
 static int test01(void)
 {
-	return 0;
+	struct cspace *cspace;
+	if (lcd_mk_cspace(&cspace))
+		LCD_FAIL("mk cspace");
+	lcd_rm_cspace(cspace);
+	LCD_PASS();
+}
+
+static int test02(void)
+{
+	struct cspace *cspace;
+	int ret;
+	if (lcd_mk_cspace(&cspace))
+		LCD_FAIL("mk cspace");
+	ret = lcd_lock_cspace(cspace);
+	if (ret)
+		LCD_FAIL("lock cspace returned %d", ret);
+	lcd_unlock_cspace(cspacse);
+	lcd_rm_cspace(cspace);
+	LCD_PASS();
 }
 
 static int api_tests(void)
 {
 	if (test01())
+		return -1;
+	if (test02())
 		return -1;
 	LCD_MSG("all api tests passed!");
 	return 0;

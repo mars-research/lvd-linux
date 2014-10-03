78f016d1404a2f839bb20e472dc135fb071af362
Sarah Spall <sspall10@gmail.com>
2014-10-02 22:51:05 -0600

    scope init code

--- tools/lcd/idl/lcd_ast.cpp
+++ tools/lcd/idl/lcd_ast.cpp
@@ -1,3 +1,20 @@
 #include "lcd_ast.h"
 
+Scope::Scope()
+{
+  // init builtin int types
+  builtin = new map<PrimType, Type>();
+  // instert for each builtin in type, add size to type if not done alreayd
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kChar, false, sizeof("char"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kChar, true, sizeof("char"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kShort, false, sizeof("short"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kShort, true, sizeof("short"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kInt, false, sizeof("int"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kInt, true, sizeof("int"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kLong, false, sizeof("long"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kLong, true, sizeof("long"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kLongLong, false, sizeof("long long"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kLongLong, true, sizeof("long long"))));
+  builtin->insert( std::pair<int,Type>(1, new IntegerType(kCapability, false, sizeof("int"))));
+}
 

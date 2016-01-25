cac858700d1c5ee949f7262b252945a3def75b7b
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2016-01-25 15:27:04 -0700

    generalized-allocator: Start LCD page allocator.
    Move old LCD code into different folder. Using lcd-domains folder
    now to mirror kernel source.

---
+++ lcd-domains/liblcd/lcd-domains/allocator.c
@@ -0,0 +1,11 @@
+/*
+ * allocator.c
+ *
+ * LCD page allocator implementation.
+ *
+ * Draws from design and code of Linux buddy allocator.
+ *
+ * Copyright: University of Utah
+ */
+
+#include <lcd-domains/liblcd.h>

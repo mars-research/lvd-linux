8368c6ca0df0e0354f179e064a4bd7c17276b0d7
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2016-03-13 15:13:14 -0600

    libasync-integration: Add libasync submodule, set up lib builds.
    Add libfipc and libasync builds. The version of libfipc we use is
    in the submodule of libasync.

--- lcd-domains/scripts/build_all.sh
+++ lcd-domains/scripts/build_all.sh
@@ -1,7 +1,11 @@
 #! /bin/bash -e
 
 make libcap.microkernel $@
+make libfipc.microkernel $@
+make libasync.microkernel $@
 make microkernel $@
 make libcap.liblcd $@
+make libfipc.liblcd $@
+make libasync.liblcd $@
 make liblcd $@
 make test_mods $@

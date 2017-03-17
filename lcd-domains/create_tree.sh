d1869bc9f816109a62c2878be4097cf3be937017
Vikram Narayanan <vikram186@gmail.com>
2017-03-17 08:54:43 -0600

    lcd/create_tree: Fix generated file names
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

--- lcd-domains/create_tree.sh
+++ lcd-domains/create_tree.sh
@@ -25,12 +25,12 @@ create_files() {
     mkdir -p ${MODULE}/{boot,${LCD}/glue,${KLCD}/glue};
     # root level files followed by boot, LCD, KLCD module files
     # Not touching Kbuild, they are written directly in the below functions
-    touch ${MODULE}/{cap.c,glue_helper.h} \
+    touch ${MODULE}/{${MODULE}_cap.c,${MODULE}_common.h} \
             ${MODULE}/boot/main.c \
             ${MODULE}/${LCD}/{main.c,${MODULE}_caller.h} \
-            ${MODULE}/${LCD}/glue/{${MODULE}_caller,dispatch}.c \
+            ${MODULE}/${LCD}/glue/{${MODULE}_caller,${MODULE}_caller_dispatch}.c \
             ${MODULE}/${KLCD}/{main.c,${MODULE}_callee.h} \
-            ${MODULE}/${KLCD}/glue/{${MODULE}_callee,dispatch}.c \
+            ${MODULE}/${KLCD}/glue/{${MODULE}_callee,${MODULE}_callee_dispatch}.c \
             ${MODULE}/${KLCD}/glue/${MODULE}_callee.lds.S
 }
 
@@ -52,8 +52,8 @@ klcd_kbuild() {
     # KLCD kbuild file
     echo -e "obj-m += lcd_test_mod_${MODULE}_${KLCD}.o\n\nlcd_test_mod_${MODULE}_${KLCD}-y\t\t+= main.o\n" > ${MODULE}/${KLCD}/Kbuild;
     echo -e "# glue code\nlcd_test_mod_${MODULE}_${KLCD}-y\t\t+= \$(addprefix glue/, ${MODULE}_callee.o \\" >> ${MODULE}/${KLCD}/Kbuild;
-    echo -e "\t\t\t\t\t\tdispatch.o )" >> ${MODULE}/${KLCD}/Kbuild;
-    echo -e "lcd_test_mod_${MODULE}_${LCD}-y\t\t+= \$(addprefix ../, cap.o" >> ${MODULE}/${KLCD}/Kbuild;
+    echo -e "\t\t\t\t\t\t${MODULE}_caller_dispatch.o )\n" >> ${MODULE}/${KLCD}/Kbuild;
+    echo -e "lcd_test_mod_${MODULE}_${KLCD}-y\t\t+= \$(addprefix ../, ${MODULE}_cap.o)\n" >> ${MODULE}/${KLCD}/Kbuild;
     echo -e "cppflags-y += \$(NONISOLATED_CFLAGS)\n" >> ${MODULE}/${KLCD}/Kbuild;
     echo -e "extra-y += glue/${MODULE}_callee.lds\n" >> ${MODULE}/${KLCD}/Kbuild;
     echo -e "ldflags-y += -T \$(LCD_TEST_MODULES_BUILD_DIR)/${MODULE}/${KLCD}/glue/${MODULE}_callee.lds\n" >> ${MODULE}/${KLCD}/Kbuild;
@@ -66,8 +66,8 @@ lcd_kbuild() {
     echo -e "# Original code\nlcd_test_mod_${MODULE}_${LCD}-y\t\t+= " >> ${MODULE}/${LCD}/Kbuild;
     echo -e "\n\nlcd_test_mod_${MODULE}_${LCD}-y\t\t+= \$(LIBLCD)\n" >> ${MODULE}/${LCD}/Kbuild;
     echo -e "# glue code\nlcd_test_mod_${MODULE}_${LCD}-y\t\t+= \$(addprefix glue/, ${MODULE}_caller.o \\" >> ${MODULE}/${LCD}/Kbuild;
-    echo -e "\t\t\t\t\t\tdispatch.o )" >> ${MODULE}/${LCD}/Kbuild;
-    echo -e "lcd_test_mod_${MODULE}_${LCD}-y\t\t+= \$(addprefix ../, cap.o" >> ${MODULE}/${LCD}/Kbuild;
+    echo -e "\t\t\t\t\t\t${MODULE}_callee_dispatch.o )\n" >> ${MODULE}/${LCD}/Kbuild;
+    echo -e "lcd_test_mod_${MODULE}_${LCD}-y\t\t+= \$(addprefix ../, ${MODULE}_cap.o)\n" >> ${MODULE}/${LCD}/Kbuild;
     echo -e "ccflags-y += \$(ISOLATED_CFLAGS)\n" >> ${MODULE}/${LCD}/Kbuild;
 }
 
@@ -76,4 +76,4 @@ main_kbuild;
 boot_kbuild;
 klcd_kbuild;
 lcd_kbuild;
-echo "Done ... !!";
+echo "Done ...!!";

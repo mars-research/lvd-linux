ced45b2e4a2cb2177bdc96e2b847b18f4576af44
Vikram Narayanan <vikram186@gmail.com>
2019-03-26 15:35:19 -0500

    test_mods/ixgbe_vmfunc: Make a copy of ixgbe module

---
+++ lcd-domains/test_mods/ixgbe_vmfunc/ixgbe_lcd/ixgbe_caller.h
@@ -0,0 +1,103 @@
+#ifndef __IXGBE_CALLER_H__
+#define __IXGBE_CALLER_H__
+
+#include "../ixgbe_common.h"
+
+void dispatch_sync_loop(void);
+int dispatch_async_loop(struct thc_channel *_channel,
+		struct fipc_message *message,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int glue_ixgbe_init(void);
+void glue_ixgbe_exit(void);
+int ndo_open_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_stop_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int ndo_start_xmit_bare_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int ndo_start_xmit_clean_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int ndo_start_xmit_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_set_rx_mode_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_validate_addr_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_set_mac_address_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_change_mtu_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_tx_timeout_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_set_tx_maxrate_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int ndo_get_stats64_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int probe_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int remove_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int sync_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+int unsync_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int ixgbe_service_event_schedule_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int trigger_dump_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int poll_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+int prep_channel_callee(struct fipc_message *_request,
+		struct thc_channel *_channel,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep);
+
+#endif /* __IXGBE_CALLER_H__ */
---
+++ lcd-domains/test_mods/ixgbe_vmfunc/net_klcd/glue/ixgbe_callee_dispatch.c
@@ -0,0 +1,415 @@
+#include <lcd_config/pre_hook.h>
+#include <liblcd/liblcd.h>
+#include "../ixgbe_callee.h"
+#include <lcd_config/post_hook.h>
+
+#define trace(x) LIBLCD_MSG("net got " #x " msg")
+
+int dispatch_sync_loop(void)
+{
+	int fn_type = lcd_r0();
+
+	switch (fn_type) {
+
+	default:
+		LIBLCD_ERR("unexpected function label %d", fn_type);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+int dispatch_async_loop(struct thc_channel *_channel,
+		struct fipc_message *message,
+		struct glue_cspace *cspace,
+		struct cptr sync_ep)
+{
+	int fn_type;
+	fn_type = async_msg_get_fn_type(message);
+	switch (fn_type) {
+		case __PCI_REGISTER_DRIVER:
+			trace(__PCI_REGISTER_DRIVER);
+			return __pci_register_driver_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_UNREGISTER_DRIVER:
+			trace(PCI_UNREGISTER_DRIVER);
+			return pci_unregister_driver_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case ALLOC_ETHERDEV_MQS:
+			trace(ALLOC_ETHERDEV_MQS);
+			return alloc_etherdev_mqs_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case REGISTER_NETDEV:
+			trace(REGISTER_NETDEVICE);
+			return register_netdev_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case ETHER_SETUP:
+			trace(ETHER_SETUP);
+			return ether_setup_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case ETH_MAC_ADDR:
+			trace(ETH_MAC_ADDR);
+			return eth_mac_addr_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case ETH_VALIDATE_ADDR:
+			trace(ETH_VALIDATE_ADDR);
+			return eth_validate_addr_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case FREE_NETDEV:
+			trace(FREE_NETDEV);
+			return free_netdev_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_CARRIER_OFF:
+			trace(NETIF_CARRIER_OFF);
+			return netif_carrier_off_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_CARRIER_ON:
+			trace(NETIF_CARRIER_ON);
+			return netif_carrier_on_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_DEVICE_ATTACH:
+			trace(NETIF_DEVICE_ATTACH);
+			return netif_device_attach_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_DEVICE_DETACH:
+			trace(NETIF_DEVICE_DETACH);
+			return netif_device_detach_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_SET_REAL_NUM_RX_QUEUES:
+			trace(NETIF_SET_REAL_NUM_RX_QUEUES);
+			return netif_set_real_num_rx_queues_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_SET_REAL_NUM_TX_QUEUES:
+			trace(NETIF_SET_REAL_NUM_TX_QUEUES);
+			return netif_set_real_num_tx_queues_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case CONSUME_SKB:
+			return consume_skb_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NAPI_CONSUME_SKB:
+			return napi_consume_skb_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case UNREGISTER_NETDEV:
+			trace(UNREGISTER_NETDEV);
+			return unregister_netdev_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case ETH_PLATFORM_GET_MAC_ADDRESS:
+			trace(ETH_PLATFORM_GET_MAC_ADDRESS);
+			return eth_platform_get_mac_address_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case DEV_ADDR_ADD:
+			trace(DEV_ADDR_ADD);
+			return dev_addr_add_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case DEV_ADDR_DEL:
+			trace(DEV_ADDR_DEL);
+			return dev_addr_del_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case DEVICE_SET_WAKEUP_ENABLE:
+			trace(DEVICE_SET_WAKEUP_ENABLE);
+			return device_set_wakeup_enable_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+#if 0
+		case ETH_GET_HEADLEN:
+			trace(ETH_GET_HEADLEN);
+			return eth_get_headlen_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+#endif
+		case NETIF_TX_STOP_ALL_QUEUES:
+			trace(NETIF_TX_STOP_ALL_QUEUES);
+			return netif_tx_stop_all_queues_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_TX_WAKE_ALL_QUEUES:
+			trace(NETIF_TX_WAKE_ALL_QUEUES);
+			return _netif_tx_wake_all_queues_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+
+		case PCI_DISABLE_PCIE_ERROR_REPORTING:
+			trace(PCI_DISABLE_PCIE_ERROR_REPORTING);
+			return pci_disable_pcie_error_reporting_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_BUS_READ_CONFIG_WORD:
+			trace(PCI_BUS_READ_CONFIG_WORD);
+			return pci_bus_read_config_word_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_BUS_WRITE_CONFIG_WORD:
+			trace(PCI_BUS_WRITE_CONFIG_WORD);
+			return pci_bus_write_config_word_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_CLEANUP_AER_UNCORRECT_ERROR_STATUS:
+			trace(PCI_CLEANUP_AER_UNCORRECT_ERROR_STATUS);
+			return pci_cleanup_aer_uncorrect_error_status_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_DISABLE_DEVICE:
+			trace(PCI_DISABLE_DEVICE);
+			return pci_disable_device_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_DISABLE_MSIX:
+			trace(PCI_DISABLE_MSIX);
+			return pci_disable_msix_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_ENABLE_MSIX_RANGE:
+			trace(PCI_ENABLE_MSIX_RANGE);
+			return pci_enable_msix_range_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_ENABLE_PCIE_ERROR_REPORTING:
+			trace(PCI_ENABLE_PCIE_ERROR_REPORTING);
+			return pci_enable_pcie_error_reporting_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCIE_CAPABILITY_READ_WORD:
+			trace(PCIE_CAPABILITY_READ_WORD);
+			return pcie_capability_read_word_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCIE_GET_MINIMUM_LINK:
+			trace(PCIE_GET_MINIMUM_LINK);
+			return pcie_get_minimum_link_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_ENABLE_DEVICE_MEM:
+			trace(PCI_ENABLE_DEVICE_MEM);
+			return pci_enable_device_mem_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_REQUEST_SELECTED_REGIONS:
+			trace(PCI_REQUEST_SELECTED_REGIONS);
+			return pci_request_selected_regions_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_REQUEST_SELECTED_REGIONS_EXCLUSIVE:
+			trace(PCI_REQUEST_SELECTED_REGIONS_EXCLUSIVE);
+			return pci_request_selected_regions_exclusive_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_SET_MASTER:
+			trace(PCI_SET_MASTER);
+			return pci_set_master_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_SAVE_STATE:
+			trace(PCI_SAVE_STATE);
+			return pci_save_state_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_RELEASE_SELECTED_REGIONS:
+			trace(PCI_RELEASE_SELECTED_REGIONS);
+			return pci_release_selected_regions_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_SELECT_BARS:
+			trace(PCI_SELECT_BARS);
+			return pci_select_bars_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case PCI_WAKE_FROM_D3:
+			trace(PCI_WAKE_FROM_D3);
+			return pci_wake_from_d3_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+
+		case __HW_ADDR_SYNC_DEV:
+			trace(__HW_ADDR_SYNC_DEV);
+			return __hw_addr_sync_dev_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case __HW_ADDR_UNSYNC_DEV:
+			trace(__HW_ADDR_UNSYNC_DEV);
+			return __hw_addr_unsync_dev_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+#ifdef HOST_IRQ
+		case REQUEST_IRQ:
+			trace(REQUEST_IRQ);
+			return _request_irq_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case _FREE_IRQ:
+			trace(_FREE_IRQ);
+			return _free_irq_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+#endif
+
+		case NETIF_NAPI_ADD:
+			trace(NETIF_NAPI_ADD);
+			return netif_napi_add_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_NAPI_DEL:
+			trace(NETIF_NAPI_DEL);
+			return netif_napi_del_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_WAKE_SUBQUEUE:
+			trace(NETIF_WAKE_SUBQUEUE);
+			return netif_wake_subqueue_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NETIF_RECEIVE_SKB:
+			return netif_receive_skb_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case NAPI_GRO_RECEIVE:
+			return napi_gro_receive_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case __NAPI_ALLOC_SKB:
+			trace(NAPI_ALLOC_SKB);
+			return __napi_alloc_skb_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+#if 0
+		case ETH_TYPE_TRANS:
+			return eth_type_trans_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+
+		case SKB_ADD_RX_FRAG:
+			trace(SKB_ADD_RX_FRAG);
+			return skb_add_rx_frag_callee(message,
+		_channel,
+		cspace,
+		sync_ep);
+#endif
+		default:
+			LIBLCD_ERR("unexpected function label: %d",
+					fn_type);
+			return -EINVAL;
+
+	}
+	return 0;
+
+}

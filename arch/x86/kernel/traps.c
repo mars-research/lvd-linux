dcbcbf4408aaee0e270d56c9e12f7f587efd5b5d
Anton Burtsev <aburtsev@uci.edu>
2019-04-06 15:24:26 -0500

    Fix definitions of the exception stacks
      -- Note, pagefault runs on normal stack -- crashes during boot on
         IST ... doen't make sense to debug now

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -897,7 +897,9 @@ void __init early_trap_init(void)
 void __init early_trap_pf_init(void)
 {
 #ifdef CONFIG_X86_64
-	set_intr_gate_ist(X86_TRAP_PF, page_fault, EXP_LVD_STACK);
+	//set_intr_gate_ist(X86_TRAP_PF, page_fault, EXP_LVD_STACK);
+	set_intr_gate(X86_TRAP_PF, page_fault);
+
 #endif
 }
 

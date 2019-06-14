7b3460236410b37d62967bfd6c427ed74db7d3fc
Vikram Narayanan <vikram186@gmail.com>
2019-06-13 19:13:53 -0500

    mm: Export symbol
    Used by the microkernel for translating a percpu allocated page to
    perform EPT mapping.

--- mm/percpu.c
+++ mm/percpu.c
@@ -1369,7 +1369,7 @@ phys_addr_t per_cpu_ptr_to_phys(void *addr)
 		return page_to_phys(pcpu_addr_to_page(addr)) +
 		       offset_in_page(addr);
 }
-
+EXPORT_SYMBOL(per_cpu_ptr_to_phys);
 /**
  * pcpu_alloc_alloc_info - allocate percpu allocation info
  * @nr_groups: the number of groups

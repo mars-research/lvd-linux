e4a2bdf37b218ced8a2399866aa46d0a0fa52fde
Vikram Narayanan <vikram186@gmail.com>
2019-11-13 19:06:19 -0700

    lcd_printk: Serialize printk
    The existing version inefficiently calls out for printing each
    character. Keeping this aside, prints get garbled when two threads enter
    vmfunc domain concurrently. To have sane print messages, serialize until
    we push out that buffer to the console. This is not an ideal version,
    but good enough for now.

--- lcd-domains/liblcd/lcd-domains/console.c
+++ lcd-domains/liblcd/lcd-domains/console.c
@@ -10,13 +10,23 @@
 
 #include <linux/kernel.h> /* for vsnprintf */
 #include <asm/lcd_domains/liblcd.h>
+#include <linux/mutex.h>
 
 #include <lcd_config/post_hook.h>
+
+static DEFINE_SPINLOCK(print_lock);
 
 void _lcd_printk(const char *fmt, va_list args)
 {
 	unsigned char buf[512]; /* this is probably a bit big ... */
 	unsigned char *p;
+	unsigned long flags;
+
+	/*
+	 * FIXME: Ideally one should implement ring buffers. But I wanted to
+	 * quickly spin a version to have ungarbled printks for debugging.
+	 */
+	spin_lock_irqsave(&print_lock, flags);
 	/*
 	 * Convert fmt string to chars
 	 */
@@ -30,9 +40,11 @@ void _lcd_printk(const char *fmt, va_list args)
 	 * Write null char
 	 */
 	lcd_syscall_putchar(0);
+
+	spin_unlock_irqrestore(&print_lock, flags);
 }
 
-void 
+void
 LIBLCD_FUNC_ATTR
 lcd_printk(const char *fmt, ...)
 {
